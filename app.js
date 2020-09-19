7f165565f9b3b9190a0d54886ef3b46b6f0959ae
d.zabinski@gmail.com <d.zabinski@gmail.com>
2020-09-18 22:35:00 -0400

    Configured the server view, modified the view settings I had been using.

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ const server = app.listen(port, function() {
     console.log('Node-Playground listening on http://localhost:$(port)')
 });
 
-app.engine('hbs', hbs( {
-    extname: 'hbs',
-    defaultLayout: 'layout';
-    layoutsDir: __dirname + '/views/layouts/',
-    partialsDir: __dirname + '/views/partials/'
-}));
+//Configure the view settings
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs( {extname: 'hbs'}));
+
+// Removed config items by setting the views path in Express and renaming the default
+// layour file in my project to main. This allows express-handlebars to just use the
+// default settings.
+//defaultLayout: 'layout',
+//layoutsDir: __dirname + '/views/layouts/',
+//partialsDir: __dirname + '/views/partials/'
+
+app.get('/', function(req, res) {
+    console.log(req.url, req.method);
+    res.render('home', {title: "Home"});
+});
